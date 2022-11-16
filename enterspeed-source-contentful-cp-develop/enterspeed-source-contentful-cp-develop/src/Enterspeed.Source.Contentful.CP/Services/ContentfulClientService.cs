4f0edf9ef7697a88033484e3b48f9e032bb66e77
Davidek776 <kocurdavid776@gmail.com>
2022-11-16 21:25:41 +0100

    Initial commit

---
+++ enterspeed-source-contentful-cp-develop/enterspeed-source-contentful-cp-develop/src/Enterspeed.Source.Contentful.CP/Services/ContentfulClientService.cs
@@ -0,0 +1,24 @@
+﻿using Contentful.Core;
+using System.Net.Http;
+using Contentstack.Core;
+
+namespace Enterspeed.Source.Contentful.CP.Services;
+
+public class ContentfulClientService : IContentfulClientService
+{
+    private readonly ContentfulClient _client;
+    // private readonly ContentstackClient _client;
+
+    public ContentfulClientService(IEnterspeedConfigurationService enterspeedConfigurationService)
+    {
+        var configuration = enterspeedConfigurationService.GetConfiguration();
+        var httpClient = new HttpClient();
+
+        _client = new ContentfulClient(configuration.ContentfulDeliveryApiKey, configuration.ContentfulPreviewApiKey, spaceId: configuration.ContentfulSpaceId);
+    }
+
+    public ContentfulClient GetClient()
+    {
+        return _client;
+    }
+}
---
+++ enterspeed-source-contentful-cp-develop/enterspeed-source-contentful-cp-develop/src/Enterspeed.Source.Contentful.CP/Services/FieldValueConverters/ObjectFieldValueConverter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Contentful.Core.Models.Management;
+using Enterspeed.Source.Contentful.CP.Models;
+using Enterspeed.Source.Sdk.Api.Models.Properties;
+
+namespace Enterspeed.Source.Contentful.CP.Services.FieldValueConverters;
+
+public class ObjectFieldValueConverter : IEnterspeedFieldValueConverter
+{
+    private readonly IEntityIdentityService _entityIdentityService;
+
+    public ObjectFieldValueConverter(IEntityIdentityService entityIdentityService)
+    {
+        _entityIdentityService = entityIdentityService;
+    }
+
+    public bool IsConverter(ContentfulField field)
+    {
+        return field.Type == typeof(ContentfulResource);
+    }
+
+    public IEnterspeedProperty Convert(ContentfulField field, Locale locale)
+    {
+        var value = ((ContentfulObjectField)field).GetValue();
+        var properties = new Dictionary<string, IEnterspeedProperty>
+        {
+            ["id"] = new StringEnterspeedProperty("id", _entityIdentityService.GetId(value.SystemProperties.Id, locale)),
+            ["type"] = new StringEnterspeedProperty("type", value.SystemProperties.Type),
+            ["linkType"] = new StringEnterspeedProperty("linkType", value.SystemProperties.LinkType)
+        };
+
+        return new ObjectEnterspeedProperty(field.Name, properties);
+    }
+}
